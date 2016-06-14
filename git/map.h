a365f8b39f76876addf30b93d0df11a45a15e116
jigardd <tranetech@imac.local>
2016-06-14 11:41:21 +0530

    add map

---
+++ git/map.h
@@ -0,0 +1,13 @@
+//
+//  map.h
+//  git
+//
+//  Created by Tranetech on 14/06/16.
+//  Copyright © 2016 tranetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface map : UIViewController
+
+@end
---
+++ git/map.m
@@ -0,0 +1,37 @@
+//
+//  map.m
+//  git
+//
+//  Created by Tranetech on 14/06/16.
+//  Copyright © 2016 tranetech. All rights reserved.
+//
+
+#import "map.h"
+
+@interface map ()
+
+@end
+
+@implementation map
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
