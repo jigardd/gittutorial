7af689aae2b85718d3f5f0594fcc0c4315cb2187
jigardd <tranetech@imac.local>
2016-06-14 10:31:14 +0530

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by Tranetech on 14/06/16.
+//  Copyright © 2016 tranetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by Tranetech on 14/06/16.
+//  Copyright © 2016 tranetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
